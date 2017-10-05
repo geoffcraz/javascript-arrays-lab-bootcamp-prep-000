2ee0725729f58a58a7cd12d05d48501165f5f5c4
geoffcraz <geoffcraz@gmail.com>
2017-10-05 18:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function destructivelyAppendKitten(name) {
   return (kittens.push(name))
 }
 
-function destructivelyPrependKitten(name) { 
+function destructivelyPrependKitten(name) {
   return (kittens.unshift(name))
 }
 
-function destructivelyRemoveLastKitten() { 
+function destructivelyRemoveLastKitten() {
   return (kittens.pop())
 }
 
